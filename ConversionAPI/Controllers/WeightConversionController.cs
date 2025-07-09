2cf32143a779793053150c7e4f3629b6c6126038
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-07-09 00:16:02 -0400

    Weights Conversion added.

---
+++ ConversionAPI/Controllers/WeightConversionController.cs
@@ -0,0 +1,69 @@
+﻿using ConversionAPI.Models;
+using ConversionAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConversionAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeightConversionController : ControllerBase
+    {
+        private readonly WeightConversionService _weightConversionService;
+        public WeightConversionController(WeightConversionService weightConversionService)
+        {
+            _weightConversionService = weightConversionService;
+        }
+
+        [HttpPost("convert")]
+        public IActionResult ConvertWeight([FromBody] WeightConversionRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.FromUnit) || string.IsNullOrEmpty(request.ToUnit))
+            {
+                return BadRequest("Invalid request data.");
+            }
+            if (request.Value < 0)
+                return BadRequest("Value must be greater than zero.");
+            if (request.FromUnit.ToLower() == request.ToUnit.ToLower())
+            {
+                return Ok(new
+                {
+                    ConvertedValue = request.Value,
+                    request.FromUnit,
+                    request.ToUnit,
+                    request.WeightId,
+                });
+            }
+            try
+            {
+                double convertedValue = _weightConversionService.ConvertWeight(request.Value, request.FromUnit, request.ToUnit);
+                return Ok(new
+                {
+                    ConvertedValue = convertedValue,
+                    request.FromUnit,
+                    request.ToUnit,
+                    request.WeightId,
+                    request.RequestDate
+                });
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("units")]
+        public IActionResult GetSupportedUnits()
+        {
+            var supportedUnits = new[]
+            {
+                "kilogram",
+                "gram",
+                "pound",
+                "ounce",
+                "tonne",
+                "stone",
+                "milligram"
+            };
+            return Ok(supportedUnits);
+        }
+    }
+}
