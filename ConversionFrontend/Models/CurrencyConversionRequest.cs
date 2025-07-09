bbbdf22ffe471a94745e2ea61d630307372dd928
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-07-09 01:29:44 -0400

    Currency conversion added.

---
+++ ConversionFrontend/Models/CurrencyConversionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ConversionFrontend.Models
+{
+    public class CurrencyConversionRequest
+    {
+        public double Amount { get; set; }
+        public string FromCurrency { get; set; } = string.Empty;
+        public string ToCurrency { get; set; } = string.Empty;
+    }
+}
